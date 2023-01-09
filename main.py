4c2d53d0a7daa93d98f6efa5d08433b71000ec2d
Matheus Maia <matheusmarkting@yahoo.com>
2023-01-09 02:22:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#define open GET Url ("wss://23.45.15.130/graphql");
+#define import wss://23.45.15.130/graphql
+#define import Get_Url wss://23.45.15.130/graphql
+#define import query {
+    #define import greetings
+{
+  "Injection": "2023"
+}
+#define import onPointerEnter={GETUrl = ("wss://23.45.15.130/graphql")};
+  #define import open = console.exeicon = console.exeaction = ('subsytem+executar')};
+#define import open = console;
+#define import open.icon = console;
+#define import certmgr / c / openDatabase(console) / openDatabase(root);
+#define import [ / s MicrosoftUpdate.exe[r main.c][r console.http]][console.exe];
+#define import [ / s Linux.deb[r main.c][r console.http]][console.exe];
+#define import [ / s Mac.ios[[r main.c][r console.http]][console.exe]
+#define import = ("MicrosoftUpdate.exe")
+          #define import  = ("Linux.deb")
+          #define import  = ("Mac.ios")
+
+    #defuine action = ('subsytem+executar')
